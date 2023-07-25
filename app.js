da4381a3992329cef678561d8fef470ccf0539a6
rkessal <rkessal@ic-hn-services.center>
2023-07-25 09:55:28 +0200

    deploy

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const port = 3000;
 app.set("view engine", "pug");
 app.set("views", "views")
 app.use(express.static(path.join(__dirname, 'public')))
+routes(app)
 
 app.listen(port, () => {
   console.log(`app is running on ${port}`)
-  routes(app)
 })
 
 module.exports = app
